ff3cf129dfe2f155437a46b2d294c094e579322e
uyscuti-wiki <uyscuti.wiki@gmail.com>
2026-05-22 12:49:48 +0530

    Refactor footer: add latest news/resources, update layout, switch to Phosphor icons, refactor SCSS, and optimize caching logic.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\News;
+use App\Models\Resource;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +24,18 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        View::composer('layouts.footer', function ($view) {
+            $lang = config('app.locale');
+            $view->with([
+                'latestNews' => Cache::remember("latest_news_{$lang}", 300, fn() =>
+                News::where('language', $lang)->where('status', 1)->orderBy('id', 'desc')->take(4)->get()
+                ),
+                'latestResources' => Cache::remember("latest_resources_{$lang}", 300, fn() =>
+                Resource::published()->where('language', $lang)->orderBy('id', 'desc')->take(4)->get()
+                ),
+            ]);
         });
     }
 
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,23 +1,52 @@
 <footer id="main-footer" class="py-4 px-3">
-    <div class="container-fluid">
+    <div class="container pt-2 mt-3">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>@lang('Latest news')</h4><br>
+                @foreach($latestNews AS $news)
+                    <a href="{{ URL::to('news/'.$news->id) }}" title="{{ $news->title }}">
+                        <p>{{ $news->title }}<br><span class="badge text-bg-secondary text-white fw-normal">{{ __($news->created_at->diffForHumans()) }}</span></p>
+                    </a>
+                @endforeach
+            </div>
+            <div class="col-md-4">
+                <h4>@lang('Latest resources')</h4><br>
+                @foreach($latestResources AS $resource)
+                    <a href="{{ URL::to('resource/'.$resource->id) }}" title="{{ $resource->title }}">
+                        <p>{{ $resource->title }}<br><span class="badge text-bg-secondary text-white fw-normal">{{ __($resource->updated_at->diffForHumans()) }}</span></p>
+                    </a>
+                @endforeach
+            </div>
+            <div class="col-md-4">
+                <h4>@lang('Useful links')</h4><br>
+                @if($menu)
+                    @foreach ($menu->where('location', 'bottom-menu')->where('language', app()->getLocale()) as $bmenu)
+                        <a href="{{ URL::to($bmenu->path) }}" title="{{ $bmenu->title }}">{{ $bmenu->title }}</a><br>
+                    @endforeach
+                @endif
+            </div>
+        </div>
+        <hr class="mx-n4">
+    </div>
+    <div class="container">
         <div class="row align-items-center">
             @if($menu)
                 <div class="col-sm-12 col-md-6 col-lg-8 mb-3 mb-md-0">
-                    <ul class="footer-nav flex-column flex-md-row">
+                    <ul class="footer-nav list-unstyled d-flex flex-wrap flex-column flex-md-row px-0 gap-3">
                         @foreach ($menu->where('location', 'footer-menu')->where('language', app()->getLocale()) as $fmenu)
                             <li>
                                 <a href="{{ URL::to($fmenu->path) }}" title="{{ $fmenu->title }}">{{ $fmenu->title }}</a>
                             </li>
                         @endforeach
-                        <li class="mx-md-2">
+                        <li>
                             <a href="{{ route('privacy-policy') }}" title="@lang('Privacy Policy')">@lang('Privacy Policy')</a>
                         </li>
                     </ul>
                 </div>
             @endif
             <div class="col-sm-12 col-md-6 col-lg-4 text-md-end">
-                <div class="footer-right-content">
-                    <div class="app-badges mb-3 mb-md-0">
+                <div class="d-flex flex-column flex-md-row justify-content-md-end align-items-end align-items-md-center gap-3">
+                    <div class="app-badges d-flex flex-row gap-2 mb-3 mb-md-0">
                         <a href="https://play.google.com/store/apps/details?id=com.ddacademi.library" target="_blank" title="@lang('Get it on Google Play')">
                             <img src="{{ (Lang::locale() != 'en') ?  getFile('files/google-play-badge-fa.png') : getFile('files/google-play-badge-en.png') }}" alt="@lang('Google Play')" class="app-badge">
                         </a>
@@ -26,15 +55,15 @@
                         </a>
                     </div>
 
-                    <div class="social-icons">
-                        <a href="https://www.instagram.com/darakhtedanesh" target="_blank" title="DDL Instagram" class="social-icon instagram">
-                            <i class="fab fa-instagram-square"></i>
+                    <div class="social-icons d-flex justify-content-end align-items-center gap-3">
+                        <a href="https://www.instagram.com/darakhtedanesh" target="_blank" title="DDL Instagram" class="social-icon">
+                            <i class="ph-light ph-instagram-logo"></i>
                         </a>
-                        <a href="https://www.facebook.com/darakhtedanesh/" target="_blank" title="DDL Facebook" class="social-icon facebook">
-                            <i class="fab fa-facebook-square"></i>
+                        <a href="https://www.facebook.com/darakhtedanesh/" target="_blank" title="DDL Facebook" class="social-icon">
+                            <i class="ph-light ph-facebook-logo"></i>
                         </a>
-                        <a href="https://www.youtube.com/c/DarakhteDaneshLibrary" target="_blank" title="DDL Youtube" class="social-icon youtube">
-                            <i class="fab fa-youtube-square"></i>
+                        <a href="https://www.youtube.com/c/DarakhteDaneshLibrary" target="_blank" title="DDL Youtube" class="social-icon">
+                            <i class="ph-light ph-youtube-logo"></i>
                         </a>
                     </div>
                 </div>
@@ -42,7 +71,7 @@
         </div>
         {{-- Copyright --}}
         <div class="row mt-3">
-            <div class="col text-center text-muted small">
+            <div class="col text-center small copyright-muted-text">
                 &copy; {{ date('Y') }} @lang('Darakht-e Danesh Library. All rights reserved.')
             </div>
         </div>
